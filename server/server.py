3d7813712076c75bf875a6dad89002686b653fd6
tsaryapkin <tsaryapkin@gmail.com>
2019-06-19 20:50:44 +0300

    aiogram

--- server/server.py
+++ server/server.py
@@ -2,10 +2,12 @@
 import asyncio
 import logging
 import os
-
+import aiohttp
+from lxml import etree
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
+from random import choice
 
 API_TOKEN = '824051603:AAGl_5lgRtI515dhMB1L63HnItvdsqV-5LU'
 PROJECT_NAME = os.getenv('PROJECT_NAME', 'aiogram-example')
@@ -26,10 +28,43 @@ bot = Bot(token=API_TOKEN, loop=loop)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    print(message.text)
-    await bot.send_message(message.chat.id, message.text)
+inline_keyboard = types.InlineKeyboardMarkup()
+random_button = types.InlineKeyboardButton('Получить случайную цитату', callback_data='refresh')
+inline_keyboard.add(random_button)
+
+async def get_random_bash_quote():
+    """Downloads bash.im/random page, parses it and returns random quote"""
+    bash_url = 'https://bash.im/random'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(bash_url) as resp:
+            html = await resp.text()
+
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(html, parser)
+    quote_tags = tree.xpath('//div[@class="text"]')  # Xpath is a query language for selecting specific tags
+    random_quote_tag = choice(quote_tags)
+    random_quote = '\n'.join(random_quote_tag.itertext())  # The easiest way to get text inside tag divided by br tags
+    return random_quote
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    """Handle start command"""
+    random_quote = await get_random_bash_quote()
+    await message.reply(random_quote, reply_markup=inline_keyboard)
+
+
+@dp.callback_query_handler(func=lambda cb: True)
+async def process_callback_data(callback_query: types.CallbackQuery):
+    """Handle all callback data which is being sent to bot"""
+    action = callback_query.data
+
+    if action == 'refresh':
+        message_id = callback_query.message.message_id
+        chat_id = callback_query.message.chat.id
+        random_quote = await get_random_bash_quote()
+        await bot.edit_message_text(random_quote, chat_id, message_id, reply_markup=inline_keyboard)
 
 
 async def on_startup(dp):
