fe8e544e1135771cc9accb299b15ffbeb373c507
tsaryapkin <tsaryapkin@gmail.com>
2019-06-19 20:36:19 +0300

    aiogram

--- server/server.py
+++ server/server.py
@@ -1,19 +1,47 @@
+
+import asyncio
 import logging
+import os
 
-from aiohttp import web
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
 
-from .controllers import routes
-from .database import connection
+API_TOKEN = '824051603:AAGl_5lgRtI515dhMB1L63HnItvdsqV-5LU'
+PROJECT_NAME = os.getenv('PROJECT_NAME', 'aiogram-example')
 
-logger = logging.getLogger('aiohttp.server')
+# webhook settings
+WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'
+WEBHOOK_PATH = '/webhook'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = os.environ.get('PORT')
+
+logging.basicConfig(level=logging.INFO)
+
+loop = asyncio.get_event_loop()
+bot = Bot(token=API_TOKEN, loop=loop)
+dp = Dispatcher(bot)
 
 
-async def on_start(app: web.Application):
-    await connection()
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
 
-app = web.Application()
 
-logging.basicConfig(level=logging.DEBUG)
-app.add_routes(routes)
-app.on_startup.append(on_start)
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
+
+
+def launch():
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
+                  skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
 
